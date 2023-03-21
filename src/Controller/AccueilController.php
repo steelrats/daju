b2c2385c90fe2eb9ff10e950bf8715eef2a8d15d
Prevost Anatole <anatole.prevost87@gmail.com>
2023-03-21 18:09:24 +0100

    acceuil upgrade

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        $lastUsername = $authenticationUtils->getLastUsername();
         return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+            'controller_name' => 'AccueilController', ['last_username' => $lastUsername]
         ]);
     }
 }
