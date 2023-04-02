b20ab408cb33a7a5acaa480e0879bbdbdabe8f53
Prevost Anatole <anatole.prevost87@gmail.com>
2023-04-02 21:51:28 +0200

    pagination en cour

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
