389121b202f5a0eb5c0ca79fcca8f81bcd6e8bae
steelrats <80911208+steelrats@users.noreply.github.com>
2023-04-02 21:41:44 +0200

    Home, manque pagination

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Drones;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $drones = $em->getRepository(Drones::class)->findAll();
+
             return $this->render('accueil/index.html.twig', [
-                'controller_name' => 'AccueilController'
+                'controller_name' => 'AccueilController',
+                'drones' => $drones
             ]);
         
     }
